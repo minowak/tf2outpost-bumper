93b7e71117ed15125fc3354aeac14108f3f60c1a
Michał Nowak <minowak@student.agh.edu.pl>
2013-06-08 18:31:12 +0200

    Bumping. Scanning every 30-35 minutes

--- functions.js
+++ functions.js
@@ -1,13 +1,18 @@
+// Writes
+function docWrite(msg) {
+	// TODO date time
+	var d = new Date();
+	document.write('<link rel="stylesheet" type="text/css" href="style.css">');
+	var tag = '[' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds() + ']'
+	document.write(tag + msg + '<br>');
+}
+
 function getCookies(domain, name, callback) {
     chrome.cookies.getAll({"domain": domain, "name": name}, function(cookie) {
         if(callback) {
             callback(cookie[0]);
         }
     });
-}
-
-function docWrite(msg) {
-	document.write(msg);
 }
 
 // Returns html of op trades page
@@ -23,27 +28,39 @@ function getTrades(callback) {
     xhr.send(null);
 }
 
-// call this function every 30-35 minutes
-window.setInterval(function() {
-	// Main
+function bumpTrade(link, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", link, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            callback(xhr.responseText);
+        }
+    };
+    xhr.send(null);
+}
+
+// core function
+function scanTrades() {
+	docWrite('scanning trades');
 	getCookies('tf2outpost.com', 'tf2op_userid', function(cookie) {
 		if(cookie && cookie.value) {
 			// already logged in
 	    	var userid = cookie.value;
-
 	    	// get trades
 			getTrades(function(text) {
 				var lines = text.split(/\r?\n/);
 				var regexp = /href="\/trade\/[0-9]+\/bump/g;
 
-				docWrite('Your trades:<br><br>');
-
 				for(var i = 0 ; i < lines.length ; i++) {
 					var line = lines[i];
 				 	if(line.match(regexp)) {
 				 		var bumpLink = line.substring(line.indexOf("href=") + 6, line.indexOf("bump") + 4);
-				 		docWrite('link: ' + bumpLink + '<br>');
-				 		// bump trade
+				 		docWrite('found unbumped link');
+				 		
+				 		//bump trade
+				 		bumpTrade("http://tf2outpost.com/" + bumpLink, function(text) {
+				 			docWrite('trade bumped')
+				 		});
 				 	}
 				}
 			});
@@ -52,4 +69,11 @@ window.setInterval(function() {
 			window.open('http://tf2outpost.com/login', '_newtab');
 		}
 	});
+}
+
+scanTrades();
+
+// call this function every 30-35 minutes
+window.setInterval(function() {
+	scanTrades();
 }, 1850000 + 300000 * Math.random());
